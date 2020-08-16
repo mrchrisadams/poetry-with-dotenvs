f9b065c036d679b0757ea5d1c7628e9434b8057f
Chris Adams <chris@productscience.co.uk>
2020-08-16 19:18:20 +0200

    Document the example function better

--- poetry_with_dotenvs/__init__.py
+++ poetry_with_dotenvs/__init__.py
@@ -4,7 +4,8 @@ __version__ = '0.1.0'
 
 def access_some_envs():
     """
-    A simple function used
+    A simple function used to demonstrate accessing environment
+    vars from within the contect of a virtual environment handled by Poetry
     """
     my_env_var = os.environ.get("MY_ENV_VAR")
 
